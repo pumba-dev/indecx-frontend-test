07c74bd0882a520800b6734aa6ee9f91abb0019a
pumba-dev <pumbadeveloper@gmail.com>
2023-02-05 15:06:14 -0300

    Feat: Create MainCard Component

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,74 +1,71 @@
 <template>
-  <v-main class="d-flex flex-row align-center justify-center h-screen">
-    <v-card
-      class="d-flex flex-column rounded-xl pa-15 align-center justify-center w-25"
-      min-width="500"
+  <MainCard>
+    <v-img
+      width="220"
+      class="mb-15"
+      src="@/assets/general/indecx-logomarca.svg"
+    />
+
+    <h1 class="text-h5 font-weight-bold align-self-start mb-2">Login</h1>
+    <span class="text-subtitle-2 align-self-start mb-9">
+      Bem-vindo de volta! Por favor insira seus dados.
+    </span>
+
+    <v-form
+      @submit.prevent
+      class="w-100 d-flex flex-column"
+      @keydown.enter="submitLogin"
     >
-      <v-img
-        width="220"
-        class="mb-15"
-        src="@/assets/general/indecx-logomarca.svg"
-      />
+      <p class="font-weight-bold text-secondary mb-2">Email</p>
+      <v-text-field
+        required
+        variant="outlined"
+        :error="v$.$error"
+        @blur="v$.email.$touch"
+        v-model.trim="loginFieldsData.email"
+        placeholder="Digite o seu e-mail"
+      ></v-text-field>
 
-      <h1 class="text-h5 font-weight-bold align-self-start mb-2">Login</h1>
-      <span class="text-subtitle-2 align-self-start mb-9">
-        Bem-vindo de volta! Por favor insira seus dados.
-      </span>
+      <p class="font-weight-bold text-secondary mb-2">Senha</p>
+      <v-text-field
+        required
+        variant="outlined"
+        :error="v$.$error"
+        @blur="v$.password.$touch"
+        placeholder="Insira a sua senha"
+        v-model="loginFieldsData.password"
+        :type="showPasswordField ? 'text' : 'password'"
+        @click:append-inner="showPasswordField = !showPasswordField"
+        :append-inner-icon="
+          showPasswordField ? 'mdi-eye-outline' : 'mdi-eye-off-outline'
+        "
+      ></v-text-field>
 
-      <v-form
-        @submit.prevent
-        class="w-100 d-flex flex-column"
-        @keydown.enter="submitLogin"
+      <v-btn
+        type="submit"
+        variant="text"
+        class="align-self-end text-primary text-subtitle-1 mb-5"
+        >Esqueci minha senha ></v-btn
       >
-        <p class="font-weight-bold text-secondary mb-2">Email</p>
-        <v-text-field
-          required
-          variant="outlined"
-          :error="v$.$error"
-          @blur="v$.email.$touch"
-          v-model.trim="loginFieldsData.email"
-          placeholder="Digite o seu e-mail"
-        ></v-text-field>
 
-        <p class="font-weight-bold text-secondary mb-2">Senha</p>
-        <v-text-field
-          required
-          variant="outlined"
-          :error="v$.$error"
-          @blur="v$.password.$touch"
-          placeholder="Insira a sua senha"
-          v-model="loginFieldsData.password"
-          :type="showPasswordField ? 'text' : 'password'"
-          @click:append-inner="showPasswordField = !showPasswordField"
-          :append-inner-icon="
-            showPasswordField ? 'mdi-eye-outline' : 'mdi-eye-off-outline'
-          "
-        ></v-text-field>
-
-        <v-btn
-          type="submit"
-          variant="text"
-          class="align-self-end text-primary text-subtitle-1 mb-5"
-          >Esqueci minha senha ></v-btn
-        >
-
-        <v-btn
-          type="submit"
-          :loading="loginIsLoading"
-          block
-          class="bg-primary fill-height rounded h-10 pa-4 font-weight-medium"
-          @click.prevent="submitLogin"
-          >Entrar</v-btn
-        >
-      </v-form>
-    </v-card>
-  </v-main>
+      <v-btn
+        type="submit"
+        :loading="loginIsLoading"
+        block
+        class="bg-primary fill-height rounded h-10 pa-4 font-weight-light text-capitalize"
+        @click.prevent="submitLogin"
+        >Entrar</v-btn
+      >
+    </v-form>
+  </MainCard>
 </template>
 
 <script setup>
 import { reactive, ref, computed } from "vue";
 import useVuelidate from "@vuelidate/core";
 import { required, email } from "@vuelidate/validators";
+
+import MainCard from "@/components/general/MainCard.vue";
 
 const loginFieldsData = reactive({
   email: "",
---
+++ src/components/general/MainCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-main
+    class="d-flex flex-row align-center justify-center h-screen bg-background"
+  >
+    <v-card
+      class="elevation-2 d-flex flex-column rounded-xl pa-15 align-center justify-center w-25 bg-surface"
+      min-width="500"
+      min-height="630"
+    >
+      <slot></slot>
+    </v-card>
+  </v-main>
+</template>
+
+<script></script>
