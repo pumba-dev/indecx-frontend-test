03339197e3d58e6804590342b5319fe3ee1de5c4
pumba-dev <pumbadeveloper@gmail.com>
2023-02-04 23:10:57 -0300

    Style: setup application colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,20 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  colors: {
+    background: "#FAFAFA",
+    surface: "#FFFFFF",
+    primary: "#3DB374",
+    secondary: "#242424",
+    error: "#FF513D",
+    "light-primary": "#4DE594",
+    "light-secondary": "#96EABD",
+    "primary-text": "#242424",
+    "secondary-text": "#5E5E5E",
+  },
+});
