317b1d37b96e3aeeb92bc4b346f5472b5b83f25c
pumba-dev <pumbadeveloper@gmail.com>
2023-02-07 00:58:30 -0300

    Fix: fix babel plugin to run only in production environment

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+const prodEnv = process.env.NODE_ENV == "production";
+let plugins = [];
+if (prodEnv) {
+  plugins.push("transform-remove-console");
+}
+
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  plugins,
 };
